ea2ed4af29b4ec5482b8b30cacb1c3d4bd40a07a
male-andante <andreadanteprivitera@gmail.com>
2024-12-18 00:07:32 +0100

    esercizio css completato

--- js/esercizio.js
+++ js/esercizio.js
@@ -1,5 +1,12 @@
-/*
-EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
-EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
-EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
-*/
+// Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
+
+/*window.addEventListener(onload, rimuoviTwitter(){
+
+})*/
+
+
+
+//EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+
+
+//EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
