3c9e672c69da50a9bdab15eb6560327cb95b8a37
male-andante <andreadanteprivitera@gmail.com>
2024-12-18 00:10:26 +0100

    impostati gli event listener

--- js/esercizio.js
+++ js/esercizio.js
@@ -1,12 +1,19 @@
 // Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
 
-/*window.addEventListener(onload, rimuoviTwitter(){
+/window.addEventListener(onload, rimuoviTwitter(){
 
-})*/
+})
 
 
 
 //EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
+addEventListener(onclick, rimuoviPadre(){
+
+})
 
 //EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
+
+addEventListener(onmouseover, alertAutore(){
+    
+})
