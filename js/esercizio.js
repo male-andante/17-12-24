6a1b7f2d312500e76b470d5d0ba5a053089b3cb6
male-andante <andreadanteprivitera@gmail.com>
2024-12-19 16:14:06 +0100

    EXTRA 1 giusto (anche se non va)

--- js/esercizio.js
+++ js/esercizio.js
@@ -1,19 +1,28 @@
 // Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
+const rimuoviTwitter = function() {
+    const twitterLink = document.querySelector("aside .list-unstyled li:nth-child(2) a")
+    console.log(twitterLink)
+    twitterLink.style.display = "none"
+    if (twitterLink) {
+        twitterLink.style.display = "none";
+        console.log("Link Twitter nascosto!"); // Debug
+    } else {
+        console.log("Elemento non trovato!"); // Debug
+    }
+    }
 
-/window.addEventListener(onload, rimuoviTwitter(){
-
-})
+window.addEventListener("load", rimuoviTwitter)
 
 
 
 //EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
-addEventListener(onclick, rimuoviPadre(){
+/*addEventListener(onclick, rimuoviPadre(){
 
 })
 
 //EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
 
 addEventListener(onmouseover, alertAutore(){
-    
-})
+
+})*/
